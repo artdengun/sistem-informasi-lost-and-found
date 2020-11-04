36a5fcafad7efef084ee08edcc8815e547a5973f
Deni Gunawan <artdengun@gmail.com>
2020-11-04 21:22:18 +0700

    sistem informasi lost and found

---
+++ src/main/java/com/kpaydeveloper/sistem/informasi/lost/and/found/SistemInformasiLostAndFoundApplication.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sistem.informasi.lost.and.found;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiLostAndFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiLostAndFoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpaydeveloper/sistem/informasi/lost/and/found/SistemInformasiLostAndFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sistem.informasi.lost.and.found;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiLostAndFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
